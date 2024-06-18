ead237c8f04dea12ad0a28f3cd732dfeb6f34c56
RicardoPoBa <jporrasbarrantes@gmail.com>
2024-06-17 20:49:18 -0600

    Revert "Pages"
    This reverts commit 4aca56dca640c1165464ce033196eff58ec89550.

--- paginas/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { buttonCSS } from "../stylesCSSCustom/stylesCSSCustom"
-
-export default function Login() {
-  return (
-    <div className="">
-        <h1 className={`hover:bg-blue-700 ${buttonCSS}`}> Login</h1>
-    </div>
-  )
-}
